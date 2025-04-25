b2d23a092353865b5ad1b6508e2813916fc55616
pavanchakravarthi999 <55144583+pavanchakravarthi999@users.noreply.github.com>
2025-04-25 09:31:52 +0530

    Time: 11 ms (82.95%), Space: 20.8 MB (63.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,9 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        
+class Solution:
+  def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    sMap = defaultdict(list)
+
+    for s in strs:
+      sorted_s = "".join(sorted(s))
+      sMap[sorted_s].append(s)
+    
+    return list(sMap.values())
